d7e49a0333841ac9beee7b59336720242e10b5bb
Saloni Sharma <68701930+saloni1966@users.noreply.github.com>
2020-10-28 15:04:42 +0530

    first commit

---
+++ Box.js
@@ -0,0 +1,25 @@
+class Box{
+
+    constructor(x,y,width,height){
+
+        var options={
+            isStatic:true
+        }
+
+        this.body=Bodies.rectangle(x,y,width,height);
+        this.w= width;
+        this.h=height;
+        World.add(world,this.body);
+
+    }
+
+    display(){
+
+        var pos = this.body.position;
+        rectMode(CENTER);
+        fill("red");
+        rect(pos.x,pos.y,this.w,this.h);
+    }
+
+
+}
